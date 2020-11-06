4aee6709bf335e53f7b39d2dfa8835d86344de45
Jake Haberle <jake.haberle@gmail.com>
2020-11-06 00:11:44 -0700

    added procfile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.I || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
